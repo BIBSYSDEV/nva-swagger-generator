41a60ef0a8771b55e6242ef63c046896c434586d
Sondre Vestad <sondre.vestad@tietoevry.com>
2022-12-02 22:41:55 +0100

    initial commit

---
+++ src/main/java/no/sikt/generator/GenerateDocsHandler.java
@@ -0,0 +1,36 @@
+package no.sikt.generator;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import nva.commons.core.JacocoGenerated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.apigateway.ApiGatewayAsyncClient;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class GenerateDocsHandler implements RequestStreamHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GenerateDocsHandler.class);
+    private final ApiGatewayAsyncClient apiGatewayClient;
+
+    @JacocoGenerated
+    public GenerateDocsHandler() {
+        this.apiGatewayClient = ApiGatewayAsyncClient.builder().build();
+    }
+
+    public GenerateDocsHandler(ApiGatewayAsyncClient client) {
+        this.apiGatewayClient = client;
+    }
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) {
+        try {
+            var apis = apiGatewayClient.getRestApis().get();
+            logger.info(apis.toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/test/java/no/sikt/generator/GenerateDocsHandlerTest.java
@@ -0,0 +1,47 @@
+package no.sikt.generator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.when;
+import java.util.concurrent.CompletableFuture;
+import nva.commons.logutils.LogUtils;
+import nva.commons.logutils.TestAppender;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mockito;
+import software.amazon.awssdk.services.apigateway.ApiGatewayAsyncClient;
+import software.amazon.awssdk.services.apigateway.model.GetRestApisResponse;
+import software.amazon.awssdk.services.apigateway.model.RestApi;
+
+class GenerateDocsHandlerTest {
+
+    private final ApiGatewayAsyncClient client = Mockito.mock(ApiGatewayAsyncClient.class);
+
+    private final GenerateDocsHandler handler = new GenerateDocsHandler(client);
+
+    @BeforeEach
+    public void setup() {
+        var restApi1 = RestApi.builder().name("First API").build();
+        var restApi2 = RestApi.builder().name("Second API").build();
+
+        var response = GetRestApisResponse.builder().items(
+            restApi1, restApi2
+        ).build();
+
+        when(client.getRestApis()).thenReturn(CompletableFuture.completedFuture(response));
+    }
+
+    @Test
+    public void shouldHaveConstrcutorWithNoArgument() {
+        Executable action = () -> new GenerateDocsHandler();
+    }
+
+    @Test
+    public void shouldLogAPIsWhenInvoked() {
+        TestAppender logger = LogUtils.getTestingAppenderForRootLogger();
+        handler.handleRequest(null, null, null);
+        assertThat(logger.getMessages(), containsString("First API"));
+    }
+
+}
