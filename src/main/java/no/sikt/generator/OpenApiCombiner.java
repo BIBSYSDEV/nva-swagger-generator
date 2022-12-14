61f2733b526794336c497fa1dcc06f8d673a2b5f
Sondre Vestad <sondre.vestad@tietoevry.com>
2022-12-14 11:03:46 +0100

    fixed PMD issues

--- src/main/java/no/sikt/generator/OpenApiCombiner.java
+++ src/main/java/no/sikt/generator/OpenApiCombiner.java
@@ -1,21 +1,23 @@
 package no.sikt.generator;
 
+import static no.sikt.generator.OpenApiUtils.addTag;
+import static no.sikt.generator.OpenApiUtils.getAllOperationsFromPathItem;
+import static no.sikt.generator.OpenApiUtils.getResourcePath;
+import static no.sikt.generator.OpenApiUtils.hasPath;
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.tags.Tag;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import io.swagger.v3.oas.models.OpenAPI;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.text.CaseUtils;
@@ -43,98 +45,75 @@ public class OpenApiCombiner {
         return new Server().url(mainServer.getUrl().replace("/{basePath}",""));
     }
 
-    @SuppressWarnings({"PMD.CognitiveComplexity", "PMD.NPathComplexity"})
     public OpenAPI combine() {
 
         this.baseTemplate.setServers(List.of(findMainServer()));
 
+        if (this.baseTemplate.getComponents() == null) {
+            this.baseTemplate.setComponents(new Components());
+        }
+
         renameDuplicateSchemas();
 
         this.others.stream().forEach(api -> {
-
-            var resource = api.getServers().get(0).getVariables().get("basePath").getDefault().replace("/","");
-
-            this.baseTemplate.addTagsItem(
-                new Tag()
-                    .name(api.getInfo().getTitle())
-                    .description(api.getInfo().getDescription())
-                    .externalDocs(new ExternalDocumentation().url("example.org").description("External doc"))
-            );
-
-            if (api.getComponents() != null) {
-                if (this.baseTemplate.getComponents() == null) {
-                    this.baseTemplate.setComponents(new Components());
-                }
-
-                if (api.getComponents().getParameters() != null) {
-                    for (Entry<String, Parameter> parameter : api.getComponents().getParameters().entrySet()) {
-                        this.baseTemplate.getComponents().addParameters(parameter.getKey(), parameter.getValue());
-                    }
-                }
-
-                if (api.getComponents().getSchemas() != null) {
-                    for (Entry<String, Schema> schema : api.getComponents().getSchemas().entrySet()) {
-                        mergeScheme(baseTemplate, schema);
-                    }
-                }
-
-                if (api.getComponents().getSecuritySchemes() != null) {
-                    var entrySet = api.getComponents().getSecuritySchemes().entrySet();
-                    for (Entry<String, SecurityScheme> securitySchemeEntry : entrySet) {
-                        mergeSecuritySchemes(baseTemplate, securitySchemeEntry);
-                    }
-                }
-            }
-
-
-            //api.getServers().stream().forEach( apiServer -> this.baseTemplate.addServersItem(apiServer));
-
-            for (Entry<String, PathItem> path : api.getPaths().entrySet()) {
-                if (path.getValue().getGet() != null) {
-                    path.getValue().getGet().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getPost() != null) {
-                    path.getValue().getPost().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getHead() != null) {
-                    path.getValue().getHead().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getDelete() != null) {
-                    path.getValue().getDelete().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getPatch() != null) {
-                    path.getValue().getPatch().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getPut() != null) {
-                    path.getValue().getPut().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getTrace() != null) {
-                    path.getValue().getTrace().addTagsItem(api.getInfo().getTitle());
-                }
-                if (path.getValue().getOptions() != null) {
-                    path.getValue().getOptions().addTagsItem(api.getInfo().getTitle());
-                }
-
-                var newPathKey = "/" + resource + path.getKey();
-                if (this.baseTemplate.getPaths() != null && this.baseTemplate.getPaths().get(newPathKey) != null) {
-                    throw new IllegalStateException("Path " + newPathKey + " already exists");
-                }
-
-                this.baseTemplate.path(newPathKey, path.getValue());
-            }
-
-            if (api.getSecurity() != null) {
-                for (SecurityRequirement securityRequirement : api.getSecurity()) {
-                    logger.info("adding security req {}", securityRequirement.toString());
-                    this.baseTemplate.addSecurityItem(securityRequirement);
-                }
-            }
-
-
-
+            this.baseTemplate.addTagsItem(OpenApiUtils.convertInfoToTag(api.getInfo()));
+            mergeComponents(api);
+            mergePaths(api);
+            mergeSecurity(api);
         });
 
         return baseTemplate;
+    }
+
+    private void mergeSecurity(OpenAPI api) {
+        if (api.getSecurity() != null) {
+            for (SecurityRequirement securityRequirement : api.getSecurity()) {
+                logger.info("adding security req {}", securityRequirement.toString());
+                this.baseTemplate.addSecurityItem(securityRequirement);
+            }
+        }
+    }
+
+    private void mergePaths(OpenAPI api) {
+        var title = api.getInfo().getTitle();
+        var resourcePath = getResourcePath(api);
+
+        for (Entry<String, PathItem> path : api.getPaths().entrySet()) {
+
+            getAllOperationsFromPathItem(path.getValue()).forEach(op -> addTag(op, title));
+
+            var newPathKey = "/" + resourcePath + path.getKey();
+            if (hasPath(baseTemplate, newPathKey)) {
+                throw new IllegalStateException("Path " + newPathKey + " already exists");
+            }
+
+            this.baseTemplate.path(newPathKey, path.getValue());
+        }
+    }
+
+    private void mergeComponents(OpenAPI api) {
+        if (api.getComponents() == null) {
+            return;
+        }
+
+        if (api.getComponents().getParameters() != null) {
+            for (Entry<String, Parameter> parameter : api.getComponents().getParameters().entrySet()) {
+                this.baseTemplate.getComponents().addParameters(parameter.getKey(), parameter.getValue());
+            }
+        }
+
+        if (api.getComponents().getSchemas() != null) {
+            for (Entry<String, Schema> schema : api.getComponents().getSchemas().entrySet()) {
+                mergeScheme(baseTemplate, schema);
+            }
+        }
+
+        if (api.getComponents().getSecuritySchemes() != null) {
+            var entrySet = api.getComponents().getSecuritySchemes().entrySet();
+            for (Entry<String, SecurityScheme> securitySchemeEntry : entrySet) {
+                mergeSecuritySchemes(baseTemplate, securitySchemeEntry);
+            }
+        }
     }
 
     private Set<String> findDuplicateSchemaNames() {
@@ -182,7 +161,7 @@ public class OpenApiCombiner {
 
     private void renameSchemaRef(OpenAPI target, String oldName, String newName) {
         target.getPaths().forEach((key, value) -> {
-            OpenApiUtils.getAllOperationsFromPathItem(value).forEach(pathOperation -> {
+            getAllOperationsFromPathItem(value).forEach(pathOperation -> {
                 pathOperation.getResponses().entrySet().forEach(response -> {
                     response.getValue().getContent().entrySet().forEach(content -> {
                         var oldRef = content.getValue().getSchema().get$ref();
--- src/main/java/no/sikt/generator/OpenApiUtils.java
+++ src/main/java/no/sikt/generator/OpenApiUtils.java
@@ -1,7 +1,10 @@
 package no.sikt.generator;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -11,6 +14,22 @@ public final class OpenApiUtils {
 
     private OpenApiUtils() {
 
+    }
+
+    public static void addTag(Operation operation, String tag) {
+        operation.addTagsItem(tag);
+    }
+
+    public static String getResourcePath(OpenAPI api) {
+        return api.getServers().get(0).getVariables().get("basePath").getDefault().replace("/","");
+    }
+
+    public static boolean hasPath(OpenAPI api, String pathKey) {
+        return api.getPaths() != null && api.getPaths().get(pathKey) != null;
+    }
+
+    public static Tag convertInfoToTag(Info info) {
+        return new Tag().name(info.getTitle()).description(info.getDescription());
     }
 
     static List<Operation> getAllOperationsFromPathItem(PathItem pathItem) {
