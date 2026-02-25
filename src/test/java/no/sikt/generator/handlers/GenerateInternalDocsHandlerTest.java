0919a1f6ef133dea5c512999a35c3340ac761a4f
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-02-25 09:25:23 +0100

    fix: Use GitHub schema components by default

--- src/test/java/no/sikt/generator/handlers/GenerateInternalDocsHandlerTest.java
+++ src/test/java/no/sikt/generator/handlers/GenerateInternalDocsHandlerTest.java
@@ -312,6 +312,19 @@ class GenerateInternalDocsHandlerTest {
     }
 
     @Test
+    void shouldOverrideExistingSchemasFromGithub() {
+        setupTestCasesFromFilesWithGithubOpenapi("schema-override", List.of(Pair.of("api.yaml", Optional.of(
+            "github.yaml"))));
+
+        handler.handleRequest(null, null, null);
+        var openApi = readGeneratedOpenApi();
+
+        var resourceResponse = openApi.getComponents().getSchemas().get("ResourceResponse");
+        var idProperty = (io.swagger.v3.oas.models.media.Schema<?>) resourceResponse.getProperties().get("id");
+        assertThat(idProperty.getExample(), is(equalTo("12345")));
+    }
+
+    @Test
     void shouldSortSchemasAlphabetically() {
         setupNvaMocks();
 
